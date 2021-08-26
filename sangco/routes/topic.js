fb32af40a66233db787350cf0b150559f319b3c8
FonDitbul <niki7084@naver.com>
2021-08-26 23:07:01 +0900

    접근제어 - 글 삭제

--- sangco/routes/topic.js
+++ sangco/routes/topic.js
@@ -65,30 +65,39 @@ router.post('/create_process', (req,res)=>{
 
 router.get('/update/:pageId', (req, res) => {
     // fs.readdir('./data', function(error, filelist) {
-    var filteredId = path.parse(req.params.pageId).base;
-    fs.readFile(`data/${filteredId}`, 'utf8',
-        function(err, data){
-            let list = template.List(req.list)
-            var title = req.params.pageId;
-            var description = data;
-            let formTag = '' + // post 방식으로 서버에 데이터 전송하는 태그
-                `<form action="/topic/update_process" method="post">` +
-                `<input type="hidden" name="id" value="${title}">` +
-                `   <p><input type="text" name="title" placeholder="title" value="${title}"></p>` +
-                `   <p>` +
-                `       <textarea name="description" placeholder="description">${description}</textarea></p>` +
-                `   <p>` +
-                `       <input type="submit"></p>` +
-                `</form>`
-            let tempBody =`<h2>${title}</h2>
-                        <p>${formTag}</p>`
-            let control = `<a href="/topic/create">create</a> <a href="/topic/update/${title}">update</a>`
+    if(!auth.isOwner(req,res)){
+        res.redirect('/');
+        return false;
+    }
+    var topic = db.get('topics').find({id:req.params.pageId}).value();
+    // var filteredId = path.parse(req.params.pageId).base; // base를 통해 상위 디렉토리를 방지하는 코드
+    var user = db.get('users').find({
+        id:topic.user_id
+    }).value();
+    if(topic.user_id !== req.user.id){
+        return res.redirect('/');
+    }
 
-            var templateStr = template.HTML(title, list, tempBody, control,
-                auth.statusUI(req,res))
-            res.send(templateStr)
-        })
-    // })
+
+    let list = template.List(req.list)
+    var title = topic.title;
+    var description = topic.description;
+    let formTag = '' + // post 방식으로 서버에 데이터 전송하는 태그
+        `<form action="/topic/update_process" method="post">` +
+        `<input type="hidden" name="id" value="${topic.id}">` +
+        `   <p><input type="text" name="title" placeholder="title" value="${title}"></p>` +
+        `   <p>` +
+        `       <textarea name="description" placeholder="description">${description}</textarea></p>` +
+        `   <p>` +
+        `       <input type="submit"></p>` +
+        `</form>`
+    let tempBody =`<h2>${title}</h2>
+                        <p>${formTag}</p>`
+    let control = `<a href="/topic/create">create</a> <a href="/topic/update/${topic.id}">update</a>`
+
+    var templateStr = template.HTML(title, list, tempBody, control,
+        auth.statusUI(req,res))
+    res.send(templateStr)
 })
 
 router.post('/update_process', (req, res) => {
@@ -96,40 +105,49 @@ router.post('/update_process', (req, res) => {
         res.redirect('/')
         return false;
     }
+
     var post = req.body;
     var title = post.title;
     var id = post.id
     var description = post.description
-    // console.log(post)
-    fs.rename(`data/${id}`, `data/${title}`, (error)=>{
-        // if(error) return
-        // response.writeHead(302, {Location:`/?id=${title}`});
-        // response.end();
-    } )
-    fs.writeFile(`data/${title}`, description, 'utf8',(err)=>{
-        if (err) throw err;
-        // 파일 생성 저장, 및 302 -> redirection 코드
-        // res.writeHead(302, {Location:`/page/${title}`});
-        // res.send()
-        res.redirect(`/topic/page/${title}`)
-    })
+
+    var topic = db.get('topics').find({id:id}).value();
+    if(topic.user_id !== req.user.id){
+        return res.redirect('/');
+    }
+    db.get('topics').find({id:id}).assign({
+        title:title, description:description
+    }).write();
+    res.redirect(`/topic/${topic.id}`);
+    // fs.rename(`data/${id}`, `data/${title}`, (error)=>{
+    //     // if(error) return
+    //     // response.writeHead(302, {Location:`/?id=${title}`});
+    //     // response.end();
+    // } )
+    // fs.writeFile(`data/${title}`, description, 'utf8',(err)=>{
+    //     if (err) throw err;
+    //     // 파일 생성 저장, 및 302 -> redirection 코드
+    //     // res.writeHead(302, {Location:`/page/${title}`});
+    //     // res.send()
+    //     res.redirect(`/topic/page/${title}`)
+    // })
 })
 
 router.post('/delete_process',(req,res)=>{
-        if(!auth.isOwner(req,res)){
-            res.redirect('/')
-            return false;
-        }
-        var post = req.body;
-        var id = post.id
-        var filteredId = path.parse(id).base;
-        fs.unlink(`data/${filteredId}`,(error)=>{
-            // res.writeHead(302, {Location:`/`});
-            // res.end();
-            res.redirect('/');
-        })
+    if(!auth.isOwner(req,res)){
+        res.redirect('/')
+        return false;
     }
-)
+    var post = req.body;
+    var id = post.id
+    var filteredId = path.parse(id).base;
+    var topic = db.get('topics').find({id:id}).value();
+    if(topic.user_id !== req.user.id){
+        res.redirect('/')
+    }
+    db.get('topics').remove({id:id}).write();
+    res.redirect('/')
+})
 
 
 router.get('/:pageId', function(req, res,next){
@@ -154,9 +172,9 @@ router.get('/:pageId', function(req, res,next){
                    <p>${sanitzedDescription}</p>
                     <p>by ${user.nickname}</p>`
     let control = `<a href="/topic/create">create</a> 
-                        <a href="/topic/update/${sanitizedTitle}">update</a>
+                        <a href="/topic/update/${topic.id}">update</a>
                         <form action="/topic/delete_process" method="post">
-                            <input type="hidden" name="id" value="${sanitizedTitle}">
+                            <input type="hidden" name="id" value="${topic.id}">
                             <input type="submit" value="delete">
                         </form>
                         `
